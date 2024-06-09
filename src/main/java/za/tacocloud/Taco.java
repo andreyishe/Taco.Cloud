014400c9bb6c3479ae19a2e1fcf1244055358719
andreyishe <andreyzamok321@gmail.com>
2024-06-09 21:23:58 +0200

    Initial commit

---
+++ src/main/java/za/tacocloud/Taco.java
@@ -0,0 +1,11 @@
+package za.tacocloud;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class Taco {
+    private String name;
+    private List<Ingredient> ingredients;
+}
---
+++ src/main/java/za/tacocloud/TacoOrder.java
@@ -0,0 +1,28 @@
+package za.tacocloud;
+
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class TacoOrder {
+
+    private String deliveryName;
+    private String deliveryStreet;
+    private String deliveryCity;
+    private String deliveryState;
+    private String deliveryZip;
+    private String ccNumber;
+    private String ccExpiration;
+    private String ccCW;
+
+    private List<Taco> tacos = new ArrayList<>();
+
+    public void addTaco(Taco taco){
+        this.tacos.add(taco);
+    }
+
+
+}
+
